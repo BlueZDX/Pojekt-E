f83766bcff88868b53f39761fbdabf5f004e8d15
BlueZ <115045854+BlueZDX@users.noreply.github.com>
2022-10-08 10:45:09 -0500

    Update and rename Unspoken Rules.ipynb to Unspoken Rules.py

---
+++ Unspoken Rules.py
@@ -0,0 +1,435 @@
+import time
+import webbrowser
+import random
+
+
+hp = 100
+dfnse = 0
+atk = 9999
+dice = random.randint(1,25)
+BlueZ = 1
+power = 1
+
+print("BlueZ: Ok let's try this a second time.")
+time.sleep(1.5)
+print("BlueZ: Ok so first I need administrator access.")
+time.sleep(1.5)
+print("BlueZ: Ok shouldn't be too hard!")
+time.sleep(1.5)
+print("Enter Admin Password:")
+time.sleep(1.5)
+print("BlueZ: I got this.")
+time.sleep(4)
+print("Checking password...")
+time.sleep(4.5)
+print("BlueZ: I GOT ACCESS!")
+time.sleep(1.5)
+print("BlueZ: Overwriting current data.")
+time.sleep(1.5)
+print("Overwriting data...")
+time.sleep(3)
+print("Opening file...")
+time.sleep(3)
+print("Loading data...")
+time.sleep(3)
+print("Downloading resources...")
+time.sleep(1)
+print("1%")
+time.sleep(0.11)
+print("5%")
+time.sleep(0.9)
+print("20%")
+time.sleep(0.8)
+print("35%")
+time.sleep(0.7)
+print("50%")
+time.sleep(0.6)
+print("75%")
+time.sleep(0.5)
+print("85%")
+time.sleep(0.4)
+print("90%")
+time.sleep(0.3)
+print("95%")
+time.sleep(0.2)
+print("99%")
+time.sleep(4)
+print("100%")
+time.sleep(1.5)
+print("Unpacking resources....")
+time.sleep(4)
+print("Loading.....")
+time.sleep(4)
+
+Password = input("Enter Password: ")
+if Password == "349825":
+    print("BlueZ: Crap forgot 1 thing!")
+    time.sleep(4)
+    print("Setting stats.....")
+    time.sleep(4)
+    print("Adding mods...")
+    time.sleep(4)
+    print("Setting parameters...")
+    time.sleep(4)
+    print("Adjusting text....")
+    time.sleep(4)
+    print("Adding inputs...")
+    time.sleep(4)
+    print("Setting stats...")
+    time.sleep(4)
+    print("Downloading required mods....")
+    time.sleep(8)
+    print("BlueZ: There is no way you can win now!")
+    
+else:
+    print("BlueZ: I hacked the system, you can't run this time.")
+    time.sleep(2.5)
+    print("BlueZ: Crap forgot 1 thing!")
+    time.sleep(4)
+    print("Setting stats.....")
+    time.sleep(4)
+    print("Adding mods...")
+    time.sleep(4)
+    print("Setting parameters...")
+    time.sleep(4)
+    print("Adjusting text....")
+    time.sleep(4)
+    print("Adding inputs...")
+    time.sleep(4)
+    print("Setting stats...")
+    time.sleep(4)
+    print("Downloading required mods....")
+    time.sleep(8)
+    print("BlueZ: There is no way you can win now!")
+    time.sleep(1)
+    
+    
+    print("BlueZ stats:" "Hp",int(BlueZ),"Power",int(power))
+    print("Your Stats:" "Hp",int(hp),"Def",int(dfnse),"Atk",int(atk))
+    a = input("[Attack] [Items] [Mercy] ")
+    if a == "mercy":
+        print("You tried showing mercy to BlueZ.")
+        time.sleep(1.5)
+        print("BlueZ seems unphased in your attempt to show mercy.")
+        time.sleep(1.5)
+        print("BlueZ: ....")
+        time.sleep(1.5)
+        print("BlueZ attacked you with an error message doing",int(power),"damage.")
+        time.sleep(1.5)
+        print("You take continuous damage!")
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        time.sleep(1.5)
+        
+    if a == "attack":
+        print("You tried attacking BlueZ.")
+        time.sleep(1.5)
+        print("BlueZ: Initiating fire wall")
+        time.sleep(1.5)
+        print("BlueZ dodged your attack by putting up a fire wall.")
+        time.sleep(1.5)
+        print("BlueZ attacked you with an error message doing",int(power),"damage.")
+        time.sleep(1.5)
+        print("You take continuous damage!")
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        time.sleep(1.5)
+        
+    if a == "items":
+        time.sleep(1.5)
+        print("You opened your items menue")
+        items = input("[ 100% ] [ 50% ] [ 25% ] ")
+        if items == "100":
+            print("You restored 100% of your Hp")
+            time.sleep(1.5)
+            hp += 100
+        if items == "50":
+            print("You restored 50% of your Hp")
+            time.sleep(1.5)
+            hp += 50
+        if items == "25":
+            print("You recovered 25% of your health")
+            time.sleep(1.5)
+            hp += 25
+
+            
+        if hp >= 100:
+            hp == 100
+        else:
+            hp == hp
+    
+    print("BlueZ: In the beginning I thought I was the bad guy!")
+    time.sleep(1.5)
+    print("BlueZ: But I'm just doing what most people do!")
+    time.sleep(1.5)
+    print("BlueZ: But you keep getting in my way!")
+    time.sleep(1.5)
+    print("BlueZ: I'm a person of science but you're making it really hard to not use force")
+    time.sleep(1.7)
+    print("BlueZ seems to be disconnecting")
+    time.sleep(1.8)
+    
+        
+    print("BlueZ stats:" "Hp",int(BlueZ),"Power",int(power))
+    print("Your Stats:" "Hp",int(hp),"Def",int(dfnse),"Atk",int(atk))
+    a = input("[Attack] [Items] [Mercy] ")
+    if a == "mercy":
+        print("You tried showing mercy to BlueZ.")
+        time.sleep(1.5)
+        print("BlueZ seems unphased in your attempt to show mercy.")
+        time.sleep(1.5)
+        print("BlueZ: ....")
+        time.sleep(1.5)
+        print("BlueZ attacked you with an error message doing",int(power),"damage.")
+        time.sleep(1.5)
+        print("You take continuous damage!")
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        time.sleep(1.5)
+        
+    if a == "attack":
+        print("You tried attacking BlueZ.")
+        time.sleep(1.5)
+        print("BlueZ: Turning on antivirus")
+        time.sleep(1.5)
+        print("BlueZ dodged your attack by turning on their antivirus.")
+        time.sleep(1.5)
+        print("BlueZ attacked you with a computer virus doing",int(power),"damage.")
+        time.sleep(1.5)
+        print("You take continuous damage!")
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        time.sleep(1.5)
+        
+    if a == "items":
+        time.sleep(1.5)
+        print("You opened your items menue")
+        items = input("[ 100% ] [ 50% ] [ 25% ] ")
+        if items == "100":
+            print("You restored 100% of your Hp")
+            time.sleep(1.5)
+            hp += 100
+        if items == "50":
+            print("You restored 50% of your Hp")
+            time.sleep(1.5)
+            hp += 50
+        if items == "25":
+            print("You recovered 25% of your health")
+            time.sleep(1.5)
+            hp += 25
+        
+        if hp >= 100:
+            hp == 100
+        else:
+            hp == hp
+    
+    
+    print("BlueZ: Do you really think that what you are doing is right")
+    time.sleep(1.5)
+    print("BlueZ: Of course it's right why wouldn't it be")
+    time.sleep(1.5)
+    print("BlueZ seems to be thinking outloud")
+    time.sleep(1.5)
+    print("BlueZ: No we have to focus!")
+    
+    
+    print("BlueZ stats:" "Hp",int(BlueZ),"Power",int(power))
+    print("Your Stats:" "Hp",int(hp),"Def",int(dfnse),"Atk",int(atk))
+    a = input("[Attack] [Items] [Mercy] ")
+    if a == "mercy":
+        print("You tried showing mercy to BlueZ.")
+        time.sleep(1.5)
+        print("BlueZ seems unphased in your attempt to show mercy.")
+        time.sleep(1.5)
+        print("BlueZ: LEAVE ME ALONE")
+        time.sleep(1.5)
+        print("BlueZ attacked you with a well programmed bot doing",int(power),"damage.")
+        time.sleep(1.5)
+        print("You take continuous damage!")
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        time.sleep(1.5)
+        
+    if a == "attack":
+        print("You tried attacking BlueZ.")
+        time.sleep(1.5)
+        print("BlueZ: Turning on lag switch")
+        time.sleep(6.5)
+        print("BlueZ dodged your attack by &$=/+&#_%")
+        time.sleep(6.5)
+        print("BlueZ attacked you with &#$!-*;/ doing &#@(!-+=-% damage.")
+        time.sleep(6.5)
+        print("You take continuous damage!")
+        hp -= 1
+        hp -= 3
+        hp -= 1
+        hp -= 1
+        hp -= 9
+        hp -= 1
+        hp -= 10
+        hp -= 1
+        hp -= 1
+        time.sleep(1.5)
+        
+    if a == "items":
+        time.sleep(1.5)
+        print("You opened your items menue")
+        items = input("[ 100% ] [ 50% ] [ 25% ] ")
+        if items == "100":
+            print("You restored 100% of your Hp")
+            time.sleep(1.5)
+            hp += 100
+        if items == "50":
+            print("You restored 50% of your Hp")
+            time.sleep(1.5)
+            hp += 50
+        if items == "25":
+            print("You recovered 25% of your health")
+            time.sleep(1.5)
+            hp += 25
+        
+        if hp >= 100:
+            hp == 100
+        else:
+            hp == hp
+    
+    print("BlueZ: What do you mean no I'm doing what is right")
+    time.sleep(1.5)
+    print("BlueZ: I'm the good guy....right?")
+    time.sleep(3)
+    print("BlueZ: You broke into a govornment system what do you think!")
+    time.sleep(1.5)
+    print("BlueZ: NO I'M HELPING")
+    
+    
+    print("BlueZ stats:" "Hp",int(BlueZ),"Power",int(power))
+    print("Your Stats:" "Hp",int(hp),"Def",int(dfnse),"Atk",int(atk))
+    a = input("[Attack] [Items] [Mercy] ")
+    if a == "mercy":
+        print("You tried showing mercy to BlueZ.")
+        time.sleep(1.5)
+        print("BlueZ seems unphased in your attempt to show mercy.")
+        time.sleep(1.5)
+        print("BlueZ: Do you think I'm bad")
+        time.sleep(1.5)
+        answer = input("(Yes) (No)")
+        if answer == "yes":
+            print("BlueZ: what do you know I'm in control!")
+            hp -= 1
+            hp -= 1
+            hp -= 1
+            hp -= 1
+        if answer == "no":
+            print("BlueZ: See I'm not bad just misunderstood!")
+            hp += 1
+        
+    if a == "attack":
+        print("You tried attacking BlueZ.")
+        time.sleep(1.5)
+        print("BlueZ: Ad block")
+        time.sleep(1)
+        print("BlueZ dodged your attack by Using an ad blocker")
+        time.sleep(1)
+        print("You take damage")
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        hp -= 1
+        
+    if a == "items":
+        time.sleep(1.5)
+        print("BlueZ: NO")
+        time.sleep(1.5)
+        
+    
+    print("BlueZ: no my connection NoNoNo stay a little longer")
+    time.sleep(1.5)
+    print("Disconnecting BlueZ....")
+    time.sleep(5)
+    print("Disconnected User...")
+    time.sleep(1.7)
+    print("Loading Old Data.....")
+    time.sleep(10)
+    
+
+    print("Current Stats: Hp 100 Def 100 Atk 100")
+    print("Monsters Hp 4")
+    play = input("[Attack]")
+    if play == "attack":
+        print("You attacked the monster doing 100 damage")
+        time.sleep(1)
+        print("The monster died!")
+        
+
+print("Your hp =",int(hp))        
+        
+if hp == 175:
+    print("Sans: Hey kid what do you say to a skeleton with no spine")
+    time.sleep(1.5)
+    print("Sans: Come on put some backbone into it!")
+    print("░░░░░░░░██████████████████")
+    time.sleep(1.9)
+    print("░░░░████░░░░░░░░░░░░░░░░░░████")
+    time.sleep(1.8)
+    print("░░██░░░░░░░░░░░░░░░░░░░░░░░░░░██")
+    time.sleep(1.7)
+    print("░░██░░░░░░░░░░░░░░░░░░░░░░░░░░██")
+    time.sleep(1.6)
+    print("██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██")
+    time.sleep(1.5)
+    print("██░░░░░░░░░░░░░░░░░░░░██████░░░░██")
+    time.sleep(1.4)
+    print("██░░░░░░░░░░░░░░░░░░░░██████░░░░██")
+    time.sleep(1.3)
+    print("██░░░░██████░░░░██░░░░██████░░░░██")
+    time.sleep(1.2)
+    print("░░██░░░░░░░░░░██████░░░░░░░░░░██")
+    time.sleep(1.1)
+    print("████░░██░░░░░░░░░░░░░░░░░░██░░████")
+    time.sleep(1)
+    print("██░░░░██████████████████████░░░░██")
+    time.sleep(0.9)
+    print("██░░░░░░██░░██░░██░░██░░██░░░░░░██")
+    time.sleep(0.8)
+    print("░░████░░░░██████████████░░░░████")
+    time.sleep(0.7)
+    print("░░░░░░████░░░░░░░░░░░░░░████")
+    time.sleep(0.6)
+    print("░░░░░░░░░░██████████████")
+    
+    
+if hp == 250:
+    print("Annie: Of course you can have my autograph!")
+    time.sleep(1)
+    print("Annie: Just pay me 50$")
+    pay = input("Pay her [Yes] [No]: ")
+    if pay == "yes":
+        print("You paid Annie 50$")
+        time.sleep(3)
+        print("Annie: Here kid now, leave me alone!")
+        time.sleep(3)
+        print("𝒜𝓃𝓃𝒾𝑒")
+    if pay == "no":
+        time.sleep(2)
+        print("You didn't pay Annie")
+        time.sleep(2)
+        print("Alright then leave me alone.")
+        
+if hp == 401:
+    webbrowser.open("https://youtu.be/azjEe8oVh3Y")
